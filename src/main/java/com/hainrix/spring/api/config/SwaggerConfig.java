05e83830ae38df2cab99f89b8c495791bd5dec5a
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-08-14 21:37:09 -0400

    feat: add config swagger

---
+++ src/main/java/com/hainrix/spring/api/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.hainrix.spring.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiDetails(){
+        return new ApiInfo(
+                "Spring API rest docs",
+                "API REST docs",
+                "1.0",
+                "",
+                new Contact("HQH", "https://www.hainrix.com","contact@hainrix.com"),
+                "",
+                "",
+                Collections.emptyList()
+        );
+    }
+}
